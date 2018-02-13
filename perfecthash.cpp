3dca5f117d99b643a03845d33d413c3195e242d1
Олександр Михед <35110565+linefire@users.noreply.github.com>
2018-02-13 19:39:20 +0200

    Update perfecthash.cpp

--- perfecthash.cpp
+++ perfecthash.cpp
@@ -8,29 +8,22 @@ int main()
 {
 	
 	string str;
-	cout << "Enter string (a-z, A-Z)[max 12 simbols]: " << endl;
-	while (cin >> str)
+	string result;
+	cout << "Enter string (a-z, A-Z, [Space]): " << endl;
+	while (getline(cin, str))
 	{
-		__int64 buf1=1, buf2=0;
-		unsigned __int64 hash=0;
+		result.clear();
 		bool nL = false;
-		if (str.size() < 13)
+		for (unsigned int i = 0; i < str.size(); ++i)
 		{
-			for (unsigned int i = 0; i < str.size(); ++i)
-			{
-				buf2 = int(str[i]);
-				if (buf2 == 32) buf2 = 0;
-				else if (buf2 > 64 && buf2 <91) buf2 -= 38;
-				else if (buf2 > 96 && buf2 <123) buf2 -= 96;
-				else {nL = true; break;}
-				hash = hash + buf1*buf2;
-				buf1 *= 54;
-			}
-			if (nL) cout << "incorect simbol" << endl;
-			cout.width(20);
-			cout.fill('0');
-			cout << hash << endl;
+				
+			if (str[i] == 32) result.append("00");
+			else if (str[i] >= 'A' && str[i] <= 'Z') result.append(to_string(int(str[i]) - 28));
+			else if (str[i] >= 'a' && str[i] <= 'z') result.append(to_string(int(str[i]) - 86));
+			else if (str[i] >= '0' && str[i] <= '9') result.append(to_string(int(str[i]) + 15));
+			else {nL = true; break;}
 		}
-		else cout << "to long string" << endl;
+		if (nL) cout << "incorect simbol" << endl;
+		else cout << result << endl;
 	}
 }
