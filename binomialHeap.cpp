3b2296b9543abb418b0db1bee9dc40dd532fdd0a
Олександр Михед <35110565+linefire@users.noreply.github.com>
2018-02-20 19:36:05 +0200

    Update and rename main.cpp to binomialHeap.cpp

--- binomialHeap.cpp
+++ binomialHeap.cpp
@@ -1,4 +1,94 @@
 #include "std.h"
+/*
+	//std.h
+	#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdalign>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#endif
+
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+	*/
+
 using namespace std;
 typedef long long ll;
 
@@ -307,34 +397,5 @@ int main(){
 	}
 	showBinomialHeap(headMain);
 	cin >> Sign;
-	/*
-	while (1){
-		string s;
-		cin >> s;
-		if (s[0] == 'c'){
-			headMain = NULL;
-		}
-		else if (s[0] == 'i'){
-			ll Number;
-			cin >> Number;
-			headMain = Insert(Number, headMain);
-		}
-		else if (s[0] == 'S'){
-			showBinomialHeap(headMain);
-		}
-		else if (s[0] == '+'){
-			Sign = 1;
-		}
-		else if (s[0] == '-'){
-			Sign = 0;
-		}
-		else if (s[0] == 'd'){
-			headMain = ExtractMin(headMain, Sign);
-		}
-		else if (s[0] == 'q'){ //used q for breaking the input loop.
-			break;
-		}
-	}
-	*/
-
+	
 }
